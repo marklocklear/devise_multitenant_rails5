9830d36c7f8e2fd33a3aaffe9477d7d677f07844
Mark Locklear <marklocklear@gmail.com>
2016-07-21 14:05:26 -0400

    add models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+	belongs_to :organization
+	has_many :tasks
+  accepts_nested_attributes_for :organization
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
